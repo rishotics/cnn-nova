f764d530a35408c2194a65360b1f3cdcaf666f7c
Rishabh Gupta <rishotics@gmail.com>
2023-05-06 23:25:23 +0530

    Initial commit

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,42 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
+require("dotenv").config();
+
+const { GOERLI_PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+
+module.exports = {
+    solidity: {
+        version: "0.8.4",
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        }
+    },
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
+            gas: 100000000,
+            blockGasLimit: 0x1fffffffffffff,
+        },
+        goerli: {
+            allowUnlimitedContractSize: true,
+            gas: 100000000,
+            blockGasLimit: 0x1fffffffffffff,
+            url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+            accounts: [`${GOERLI_PRIVATE_KEY}`],
+        },
+    },
+    namedAccounts: {
+        deployer: 0,
+    },
+    paths: {
+        deploy: "deploy",
+        deployments: "deployments",
+    },
+    mocha: {
+        timeout: 1000000
+    }
+};

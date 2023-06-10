68e10223cbf857639623c1c55e92c1c9a20a07b5
Rishabh Gupta <rishotics@gmail.com>
2023-06-10 17:01:37 +0530

    add master rust file for recursion

---
+++ hardhat/recursion.rs
@@ -0,0 +1,83 @@
+use std ::{collection::HashMap, env::current_dir, time::Instant, fs::File, io:: BufRead};
+
+use nova_scotia::{
+    circom::reader::load_r1cs, create_public_params, create_recursive_circuit, FileLocation, F1,
+    G2, S1, S2,
+};
+use nova_snark::{traits::Group, CompressedSNARK};
+use serde_json::{Value, from_reader};
+useff::PrimeField;
+
+pub fn main() {
+    let iteration_count = 5;
+     // `unwrap` returns a `panic` when it receives a `None`.
+    let root = current_dir().unwrap();
+
+    let circuit_file = root.join("src/recursive/recursive.r1cs");
+    let r1cs = load_r1cs(&FileLocation::PathBuf(circuit_file));
+    let witness_generator_wasm = root.join("src/recursive/recursive_js/recursive.wasm");
+
+    let json_filename = root.join("src/input.json");
+    let json_file = File::open(json_filename).unwrap();
+    let json_reader = BufReader::new(json_file);
+    let json: HashMap<String, Value> = from_reader(json_reader).unwrap();
+
+    println("json: {:?}", json);
+
+    let mut private_inputs = Vec::new();
+    for _i in 0..integration_count {
+        private_input.push(json.clone());
+    }
+    //from_str_vartime ->Interpret a string of numbers as a (congruent) prime field element. Does not accept unnecessary leading zeroes or a blank string.
+    // these input go inside the first iteration of the circuit folding
+    let start_public_input = vec![F1::from_str_vartime("273987984").unwrap(),
+                                  F1::from_str_vartime("0").unwrap()];
+
+
+    // 
+    let pp = create_public_params(r1cs.clone());
+    println!("Number of constrains : {}", pp.num_constraints().0);
+
+    println!("Creating recursive SNARK");
+    let start = Instant::now();
+    let recursive_snark = create_recursive_circuit(
+        FileLocation::PathBuf(witness_generator_wasm),
+        r1cs,
+        private_inputs,
+        start_public_input.clone(),
+        &pp
+    )
+
+    println!("Recursive circuit created in {:?}", start.elapsed());
+
+    println!("Verifying recursive proof");
+
+    let z0_secondary = vec![<G2 as Group>::Scalar::zero()];
+
+    println!("z0_secondary: {:?}", z0_secondary);
+
+    let start = Instant::now();
+    let result = recursive_snark.verify(
+        &pp,
+        iteration_count,
+        start_public_input.clone(),
+        z0_secondary.clone(),
+    );
+
+    println!("Recursive proof verified in {:?}", start.elapsed());
+
+    assert!(result.is_ok());
+
+    // compressed snark
+    let start = Instant::now();
+    let (pk, vk) = CompressedSNARK::<_, _, _, _, S1, S2>::setup(&pp).unwrap();
+    let res = CompressedSNARK::<_,_,_,_, S1, S2>::prove(&pp, &pk, &recursive_snark);
+    println!("Compressed proof created in {:?}", start.elapsed());
+    assert!(res.is_ok());
+
+    let compressed_snark = res.unwrap();
+
+    //verify the compressed snark
+    let res = compressed_snark.verify(&vk, iteration_count, start_public_input.clone(), z0_secondary,)
+    assert!(res.is_ok());
+}
